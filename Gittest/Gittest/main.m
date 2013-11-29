bc55d62d793c0c601a79e8513b44a5510662ba13
Димитров Евгений <evgeniy.dimitrov@customertimes.comevgeniy.dimitrov@customertimes.com>
2013-11-29 15:56:37 +0400

    origin code

---
+++ Gittest/Gittest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Gittest
+//
+//  Created by Mac on 11/29/13.
+//  Copyright (c) 2013 Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
